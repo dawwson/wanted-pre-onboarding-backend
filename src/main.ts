d556aff7bbedc0c9e7fadc244c50c14adc54b396
dawwson <shines1427@gmail.com>
2023-10-10 18:36:00 +0900

    Add: 유효성 검사 파이프 추가

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get<ConfigService>(ConfigService);
+  // 요청 유효성 검사 파이프 설정
+  app.useGlobalPipes(new ValidationPipe());
 
+  const configService = app.get<ConfigService>(ConfigService);
   const serverPort = configService.get<string>('server.port');
   await app.listen(serverPort);
 }
