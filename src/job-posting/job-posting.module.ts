0b684575cc7256b46f8dba5e0dca2d6fc46ec887
dawwson <shines1427@gmail.com>
2023-10-13 17:29:10 +0900

    Remove: DynamicRepositoryModule 삭제

--- src/job-posting/job-posting.module.ts
+++ src/job-posting/job-posting.module.ts
@@ -7,17 +7,15 @@ import { UserService } from '../user/user.service';
 import { JobPostingRepository } from '../repository/job-posting.repository';
 import { CompanyRepository } from '../repository/company.repository';
 import { UserRepository } from '../repository/user.repository';
-import { DynamicRepositoryModule } from '../repository/dynamic-repository.module';
 
 @Module({
-  imports: [
-    DynamicRepositoryModule.forRepository([
-      JobPostingRepository,
-      CompanyRepository,
-      UserRepository,
-    ]),
-  ],
   controllers: [JobPostingController],
-  providers: [JobPostingService, UserService],
+  providers: [
+    JobPostingService,
+    UserService,
+    JobPostingRepository,
+    CompanyRepository,
+    UserRepository,
+  ],
 })
 export class JobPostingModule {}
