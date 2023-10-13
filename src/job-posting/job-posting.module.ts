63af7b2b7dd379fdddb4eb30d4f79045b67b4ea2
dawwson <shines1427@gmail.com>
2023-10-13 16:44:51 +0900

    Refactor: 모듈에서 필요한 Repository들을 동적으로 셋팅해주는 다이나믹 모듈 추가

--- src/job-posting/job-posting.module.ts
+++ src/job-posting/job-posting.module.ts
@@ -7,15 +7,17 @@ import { UserService } from '../user/user.service';
 import { JobPostingRepository } from '../repository/job-posting.repository';
 import { CompanyRepository } from '../repository/company.repository';
 import { UserRepository } from '../repository/user.repository';
+import { DynamicRepositoryModule } from '../repository/dynamic-repository.module';
 
 @Module({
-  controllers: [JobPostingController],
-  providers: [
-    JobPostingService,
-    UserService,
-    JobPostingRepository,
-    CompanyRepository,
-    UserRepository,
+  imports: [
+    DynamicRepositoryModule.forRepository([
+      JobPostingRepository,
+      CompanyRepository,
+      UserRepository,
+    ]),
   ],
+  controllers: [JobPostingController],
+  providers: [JobPostingService, UserService],
 })
 export class JobPostingModule {}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,8 +3,15 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { JobPostingRepository } from '../repository/job-posting.repository';
 import { UserRepository } from '../repository/user.repository';
+import { DynamicRepositoryModule } from '../repository/dynamic-repository.module';
 
 @Module({
+  imports: [
+    DynamicRepositoryModule.forRepository([
+      UserRepository,
+      JobPostingRepository,
+    ]),
+  ],
   providers: [UserService, UserRepository, JobPostingRepository],
   exports: [UserService],
 })
