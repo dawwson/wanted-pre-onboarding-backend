74926e63c4e03d5c53b35ed50113a326381404eb
dawwson <shines1427@gmail.com>
2023-10-14 10:45:20 +0900

    Test: UserService - getOne() 단위 테스트 검증 내용 수정

--- test/unit/service/user.service.spec.ts
+++ test/unit/service/user.service.spec.ts
@@ -1,17 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { Repository } from 'typeorm';
 
-import { Role, User } from '../../../src/entity/user.entity';
-import { UserRepository } from '../../../src/repository/user.repository';
 import { UserService } from '../../../src/api/user/user.service';
 
-import { getRepositoryToken } from '@nestjs/typeorm';
+import { Role, User } from '../../../src/entity/user.entity';
+import { UserRepository } from '../../../src/repository/user.repository';
 import { JobPostingRepository } from '../../../src/repository/job-posting.repository';
 
 describe('UserService', () => {
   let userService: UserService;
   let userRepository: UserRepository;
-  let jobPostingRepository: JobPostingRepository;
 
   const mockUserRepository = {
     findById: jest.fn(),
@@ -23,56 +21,54 @@ describe('UserService', () => {
       providers: [
         UserService,
         {
-          provide: getRepositoryToken(UserRepository),
+          provide: UserRepository,
           useValue: mockUserRepository,
         },
         {
-          provide: getRepositoryToken(JobPostingRepository),
+          provide: JobPostingRepository,
           useValue: Repository,
         },
       ],
     }).compile();
 
     userService = module.get<UserService>(UserService);
-    userRepository = module.get<UserRepository>(
-      getRepositoryToken(UserRepository),
-    );
-    jobPostingRepository = module.get<JobPostingRepository>(
-      getRepositoryToken(JobPostingRepository),
-    );
+    userRepository = module.get<UserRepository>(UserRepository);
   });
 
   it('should be defined', () => {
     expect(userService).toBeDefined();
     expect(userRepository).toBeDefined();
-    expect(jobPostingRepository).toBeDefined();
   });
 
   test('getOne() : 하나의 회원 정보를 반환한다.', async () => {
     // given
     const testUserId = 1;
-    const mockRepoValue = {
-      id: testUserId,
-      email: 'test@gmail.com',
-      password: '1234',
-      name: 'test',
-      role: Role.INDIVIDUAL,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    } as User;
 
-    jest.spyOn(userRepository, 'findById').mockResolvedValue(mockRepoValue);
+    const findByIdSpy = jest
+      .spyOn(userRepository, 'findById')
+      .mockResolvedValue({
+        id: testUserId,
+        email: 'test@gmail.com',
+        password: '1234',
+        name: 'test',
+        role: Role.INDIVIDUAL,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      } as User);
 
     // when
-    const user = await userService.getOne(testUserId);
+    const result = await userService.getOne(testUserId);
 
     // then
-    expect(user).toHaveProperty('id');
-    expect(user).toHaveProperty('email');
-    expect(user).toHaveProperty('password');
-    expect(user).toHaveProperty('name');
-    expect(user).toHaveProperty('role');
-    expect(user).toHaveProperty('createdAt');
-    expect(user).toHaveProperty('updatedAt');
+    expect(findByIdSpy).toHaveBeenCalled();
+    expect(result).toMatchObject({
+      id: testUserId,
+      email: expect.any(String),
+      password: expect.any(String),
+      name: expect.any(String),
+      role: expect.anything(),
+      createdAt: expect.any(Date),
+      updatedAt: expect.any(Date),
+    });
   });
 });
